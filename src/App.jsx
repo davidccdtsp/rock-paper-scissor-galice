96231a57451013018fa27fc375dc6cc2cbc46692
david.cc <david.cc@dataspartan.com>
2023-07-07 19:03:33 +0200

    piedra papel

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,80 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+function getRandomNumber() {
+  return Math.floor(Math.random() * (2 - 0) + 0);
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [choice, setChoice] = useState('');
+  const [iaChoice, setIaChoice] = useState('');
+  const [isPlayed, setIsPlayed] = useState(false);
+  const [result, setResult] = useState('');
+
+  // Estado para determinar la fase del juego, si seleccion o desenlace
+
+  function getResult (){
+    if (choice == 'Rock' && iaChoice == 'Scissors') {
+      return 'win';
+    } else if (choice == 'Paper' && iaChoice == 'Rock') {
+      return 'win';
+    } else if (choice == 'Scissors' && iaChoice == 'Paper') {
+      return 'win';
+    } else if (choice !== iaChoice) {
+      return 'lose';
+    }
+
+    return 'tie';
+  }
+
+
+  const shapes = ['Rock', 'Paper', 'Scissors'];
+
+// debugger;
+  if (choice != '' && isPlayed) {
+    setResult(getResult());
+
+    if(result == 'win'){
+      setCount(count + 1);
+    }
+    
+    if(result == 'lose'){
+      setCount(count - 1);
+    }
+
+    setIsPlayed(false);
+  }
+
+  // console.log(shapes);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div><h1>{count}</h1></div>
+      {choice == '' ?
+        <div>{shapes.map(s =>
+          <button onClick={() => {setChoice(s); setIaChoice(shapes[getRandomNumber()]); setIsPlayed(true)}} key={s}>{s}</button>
+        )}</div>
+        :
+        <div style={{display: 'flex', flexDirection: 'row', gap: '1rem'}}>
+          <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+            <span>Player selection</span>
+            <span>{choice}</span>
+          </div>
+
+          {result == 'win' && <div>Player Win</div>}
+          {result == 'lose' && <div>Player Lose</div>}
+          {result == 'tie' && <div>Player Tie</div>}
+
+          <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+            <span>IA selection</span>
+            <span>{iaChoice}</span>
+          </div>
+          <button onClick={() => setChoice('')}>Jugar de nuevo</button>
+        </div>
+      }
     </>
   )
 }
